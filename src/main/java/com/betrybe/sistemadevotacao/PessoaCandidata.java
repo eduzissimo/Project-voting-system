3ac1005a53a505299722d7c09cea06cfdeffa493
eduzissimo <eduardo.alves14@hotmail.com>
2024-07-30 19:06:12 -0300

    Feat: Req. 2 feito

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -3,6 +3,35 @@ package com.betrybe.sistemadevotacao;
 /**
  * Abstract class for a candidate.
  */
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
+  public int numero;
+  public int votos;
 
+  /**
+   * Constructor pro candidato.
+   *
+   * @param nome - nome do candidato
+   * @param numero - número do candidato
+   */
+  public PessoaCandidata(String nome, int numero) {
+    this.nome = nome;
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void receberVoto() {
+    this.votos++;
+  }
 }
