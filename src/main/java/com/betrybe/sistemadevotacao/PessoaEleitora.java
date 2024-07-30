e55e81d56b356f5c417c2a1cb2c75f7be9e90e83
eduzissimo <eduardo.alves14@hotmail.com>
2024-07-30 19:08:00 -0300

    Feat: Req. 3 feito

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -3,6 +3,25 @@ package com.betrybe.sistemadevotacao;
 /**
  * Abstract class for a voter.
  */
-public class PessoaEleitora{
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
 
+  /**
+   * Constructor for the voter.
+   *
+   * @param nome - voter's name
+   * @param cpf - voter's CPF
+   */
+  public PessoaEleitora(String nome, String cpf) {
+    this.nome = nome;
+    this.cpf = cpf;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
